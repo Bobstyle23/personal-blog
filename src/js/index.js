3882ca11ead5c306f9bfbc5354044448d8a53eed
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-09-01 18:58:55 +0900

    Add home logic to save data into session storage

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,36 @@
+const _data = new WeakMap();
 
+class Home {
+  constructor() {
+    Home.init();
+    _data.set(this, Home.#parseData(sessionStorage.getItem("blogData")));
+  }
+
+  static #isInitialized = false;
+
+  static #parseData(data) {
+    try {
+      return JSON.parse(data) || [];
+    } catch {
+      return [];
+    }
+  }
+
+  static init() {
+    if (!this.#isInitialized) {
+      const data = require("./data.json");
+      sessionStorage.setItem("blogData", JSON.stringify(data));
+      this.#isInitialized = true;
+    }
+  }
+
+  cacheDOM() {}
+
+  bindEvents() {}
+
+  getData() {
+    return _data.get(this);
+  }
+}
+
+new Home();
